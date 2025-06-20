380078ec2bd9a3db9772af10c15c459f10db4d18
Gaurav Sharma <gauraxvx0410@gmail.com>
2025-06-20 17:45:40 +0530

    Queries DOne

--- query.sql
+++ query.sql
@@ -4,11 +4,21 @@ SELECT * FROM employees;
 -- 2. Total Salary of all Department
 SELECT department,CAST(SUM(salary) as INT) as total_salary FROM employees GROUP BY department;
 
--- 3. Highest Salary as per Gender
-SELECT gender,MAX(salary) FROM employees GROUP BY gender;
+-- 3. Average Salary of all Department
+SELECT department,CAST(AVG(salary) as INT) as total_salary FROM employees GROUP BY department;
 
--- 4. Get Average Salary as per Gender
+-- 4. Highest Salary as per Gender
+SELECT gender,MAX(salary) AS Maximum FROM employees GROUP BY gender;
 
-select gender,CAST(avg(salary)as int) from employees group by gender;
+-- 5. Lowest Salary as per Gender
+SELECT gender,MIN(salary) AS Minimum FROM employees GROUP BY gender;
 
--- 4 
+-- 6. Average Salary as per Gender
+
+SELECT gender,CAST(AVG(salary) AS INT) AS Average from employees GROUP BY gender;
+
+-- 7. Total Salary as per Gender
+SELECT gender,CAST(SUM(salary) AS INT) AS total_salary FROM employees GROUP BY gender;
+
+-- 8. Employees Location Distribution
+select work_location,count(emp_id) as Total from employees group by work_location; 
