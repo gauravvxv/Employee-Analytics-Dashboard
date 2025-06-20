7384ccad74e8dd3c04ed3b594fab5f5a5cb8b269
Gaurav Sharma <gauraxvx0410@gmail.com>
2025-06-20 13:49:13 +0530

    connect to sql

---
+++ upload_to_postgres.py
@@ -0,0 +1,17 @@
+import pandas as pd
+from sqlalchemy import create_engine
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+db_url = os.getenv('POSTGRES_URL')
+print(db_url)
+
+df = pd.read_excel('./Data/python_cleaned_staff.xlsx')
+
+engine = create_engine(db_url)
+
+df.to_sql('employees',engine,index=False,if_exists='replace')
+
+print("Upload successfully")
